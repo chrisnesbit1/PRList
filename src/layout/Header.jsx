5c9853bb1cebc331d96b2de5dd60c2c0a4c24e63
Chris Nesbit <5253445+chrisnesbit1@users.noreply.github.com>
2025-11-30 21:55:32 -0500

    simplify navigation, simplify/improve related css, and center page title (#4)

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,20 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <>
-      <h1>PRList
-        <span>
-          <input type="checkbox" id="menu-toggle" />
-          <label htmlFor="menu-toggle" className="hamburger floatRight">☰</label>
-          <ul className="menu">
-            {/* server-side routing: <li><a href="/">Pull Requests</a></li> */}
-            {/* server-side routing: <li><a href="/about">About</a></li> */}
-            <li><Link to="/">Pull Requests</Link></li>
-            <li><Link to="/about">About</Link></li>
-
-          </ul>
+    <div className="header">
+      <h1><Link to="/">PRList</Link>
+        <span className="float-right">
+            <Link to="/">Pull Requests</Link>
+            <Link to="/about">About</Link>
         </span>
       </h1>
-    </>
+    </div>
   );
 }
