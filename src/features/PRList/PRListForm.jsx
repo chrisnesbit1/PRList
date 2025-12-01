2899a6ff8158f459c4ad27d687ae3bc04db53f41
Chris Nesbit <chris.nesbit1@gmail.com>
2025-11-30 23:29:40 -0500

    boilerplate structure for PRListForm, and defining PRListPage as the parent/orchestrator of interactions between PRListForm and other future components. basic styling included

---
+++ src/features/PRList/PRListForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+export default function PRListForm() {
+    const [pat, setPat] = useState('');
+    const [reposText, setReposText] = useState('');
+
+    return (
+        <>
+            <div className="prlist-form">
+                <div className="prlist-form-repos">
+                    <label htmlFor="repos">GitHub Org/Repo(s):</label><br/>
+                    <textarea
+                        id="repos"
+                        rows="6"
+                        cols="40"
+                        value={reposText}
+                        onChange={e => setReposText(e.target.value)}
+                    />
+                </div>
+                
+                <div className="prlist-form-pat">
+                    <label htmlFor="pat">GitHub Personal Access Token:</label><br/>
+                    <input
+                        id="pat"
+                        size="48"
+                        type="password"
+                        value={pat}
+                        onChange={e => setPat(e.target.value)}
+                    />
+
+                    <br/>
+
+                    <button type="submit">Save Settings</button>
+                </div>
+            </div>
+        </>
+    );
+}
